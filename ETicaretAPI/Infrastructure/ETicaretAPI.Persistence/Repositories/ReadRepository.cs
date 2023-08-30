e96ec5758a0c6dffc8375f75f0e689efbc4e3327
29534411212 <tkalyoncuoglu@gmail.com>
2023-08-30 21:13:06 +0300

    refactored

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -21,20 +21,6 @@ namespace ETicaretAPI.Persistence.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll(bool tracking = true)
-        {
-            var query = Table.AsQueryable();
-            if (!tracking)
-                query = query.AsNoTracking();
-            return query;
-        }
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method, bool tracking = true)
-        {
-            var query = Table.Where(method);
-            if (!tracking)
-                query = query.AsNoTracking();
-            return query;
-        }
         public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
         {
             var query = Table.AsQueryable();
